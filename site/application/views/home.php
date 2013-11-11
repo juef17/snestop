2c07d829f4f62552faca38695de652f8948e53f2
Jérôme Thériault <jerther@gmail.com>
2013-11-11 01:53:26 +0000

    - Ajustement de la taille des polices pour h1 et h2 - ajout du texte de bienvenue
    git-svn-id: http://jerther.com/svn/snestop/trunk@23 ce5e4949-4d00-44cd-bea3-2e8dfc70eed9

--- site/application/views/home.php
+++ site/application/views/home.php
@@ -1,7 +1,14 @@
 <div class="container_12">
 	<div class="grid_12">
-		<h1>Welcome to the Top 100 SNES music web site!</h1>
-		<p>Here you'll find everythig snes music related! And then some.</p>
+		<h1>Greetings, and welcome to the Top SNES tracks project!</h1>
+		<p>This project aims to please the Super Nintendo or Super Famicom aficionado and the video game music lovers in two different ways:</p>
+		<ul>
+			<li>Promote musical gems from obscure and/or mediocre games;</li>
+			<li>Use rating systems to compile a statistically valid list of the best SNES tracks ever.</li>
+		</ul>
+		<p>We plan to achieve this by allowing users to listen to high quality & small sized music files, create and share playlists with anybody, and most importantly, have the SNES tracks compete in the most epic 1 vs 1 tournament ever.</p>
+		<p>More details about this project, the tracks and the rating algorithms are available in the About page.</p>
+		<p>Thank you for checking this out, and please have fun!</p>
 	</div>
 </div>
 
