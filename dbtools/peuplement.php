5503cae86c06bdd23415e458a9a22df96fd6c0a2
François Gingras <juef@hotmail.com>
2013-12-15 02:11:14 +0000

    Peuplement de games et de tracks!
    git-svn-id: http://jerther.com/svn/snestop/trunk@33 ce5e4949-4d00-44cd-bea3-2e8dfc70eed9

--- dbtools/peuplement.php
+++ dbtools/peuplement.php
@@ -5,8 +5,10 @@
 	{
 		if ($mysqli->query($query) === TRUE)
 			echo 'Query Success: ' . $query . '<br />';
-		else
+		else {
 			echo 'Query failed: ' . $mysqli->error . '<br />';
+			echo 'Query was: ' . $query . '<br>';
+		}
 	}
 
 	function EchoCount($mysqli, $table)
@@ -21,11 +23,15 @@
 		$mysqli = CreateConnection();
 		//additional deletions here!
 		EchoQuery($mysqli, 'DELETE FROM User;');
+		EchoQuery($mysqli, 'DELETE FROM Game;');
+		EchoQuery($mysqli, 'DELETE FROM Track;');
 	}
 
 	function PopulateFull()
 	{
 		PopulateUsers();
+		PopulateGames();
+		PopulateTracks();
 		//additional populations here!
 	}
 
@@ -49,6 +55,37 @@
 			VALUES($idCommunity, '$email', '" . MD5($password) . "', '$language', $canStreamMP3, $autoPlay, '$userName', $isAdmin)
 		");
 	}
+	
+	function PopulateGames()
+	{
+		$mysqli = CreateConnection();
+		include("peuplement_games.php");
+		EchoCount($mysqli, 'Game');
+	}
+	
+	function InsertGame($mysqli, $titleJap, $titleEng, $screenshotURL, $rsnFileURL)
+	{
+		EchoQuery($mysqli,"
+			INSERT INTO Game(titleJap, titleEng, screenshotURL, rsnFileURL)
+			VALUES('" . addslashes($titleJap) . "', '" . addslashes($titleEng) . "', '" . addslashes($screenshotURL) . "', 'rsn/" . addslashes($titleEng) . ".rsn')
+		");
+	}
+	
+	function PopulateTracks()
+	{
+		$mysqli = CreateConnection();
+		include("peuplement_tracks.php");
+		echo "<br>Peuplement de tracks... : ";
+		EchoCount($mysqli, 'Track');
+	}
+	
+	function InsertTrack($mysqli, $title, $length, $fadeLength, $composer, $turnedOffByAdmin, $screenshotURL, $isJingle, $glicko2RD, $glicko2rating, $glicko2sigma, $eloRating, $spcURL, $spcEncodedURL, $gameTitle)
+	{
+		$mysqli->query("
+			INSERT INTO Track(idGame, title, length, fadeLength, composer, turnedOffByAdmin, screenshotURL, isJingle, glicko2RD, glicko2rating, glicko2sigma, eloRating, spcURL, spcEncodedURL)
+			VALUES((SELECT idGame FROM game WHERE titleEng = '" . addslashes($gameTitle) . "'), '" . addslashes($title) . "', '$length', '$fadeLength', '" . addslashes($composer) . "', '$turnedOffByAdmin', '" . addslashes($screenshotURL) . "', '$isJingle', '$glicko2RD','$glicko2rating','$glicko2sigma','$eloRating', 'spc/" . addslashes($spcURL) . ".spc', '$spcEncodedURL')
+		");
+	}
 ?>
 
 
@@ -58,6 +95,8 @@
 	<body>
 		<p><a href="peuplement.php?action=clear">Vider la base de données</a></p>
 		<p><a href="peuplement.php?action=users">Peupler les utilisateurs</a></p>
+		<p><a href="peuplement.php?action=games">Peupler les games</a></p>
+		<p><a href="peuplement.php?action=tracks">Peupler les tracks</a></p>
 		<p><a href="peuplement.php?action=full">Peuplement entier</a></p>
 		<p><a href="index.php">Retour à l'outil principal</a></p>
 		<?php
@@ -67,6 +106,10 @@
 					ClearDB();
 				else if($_GET['action'] == 'users')
 					PopulateUsers();
+				else if($_GET['action'] == 'games')
+					PopulateGames();
+				else if($_GET['action'] == 'tracks')
+					PopulateTracks();
 				else if($_GET['action'] == 'full')
 					PopulateFull();
 			}
