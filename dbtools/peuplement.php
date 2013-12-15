e0210ee84e87aa9d95df12e9d944420db19d17c8
François Gingras <juef@hotmail.com>
2013-12-15 03:55:42 +0000

    Peuplement des reviews et communities
    git-svn-id: http://jerther.com/svn/snestop/trunk@39 ce5e4949-4d00-44cd-bea3-2e8dfc70eed9

--- dbtools/peuplement.php
+++ dbtools/peuplement.php
@@ -15,7 +15,7 @@
 	{
 		$result = $mysqli->query("SELECT count(*) as total from $table;");
 		$data = $result->fetch_assoc();
-		echo "Count for table $table : " . $data['total'];
+		echo "Count for table $table : " . $data['total'] . "<br>";
 	}
 
 	function ClearDB()
@@ -25,6 +25,8 @@
 		EchoQuery($mysqli, 'DELETE FROM User;');
 		EchoQuery($mysqli, 'DELETE FROM Game;');
 		EchoQuery($mysqli, 'DELETE FROM Track;');
+		EchoQuery($mysqli, 'DELETE FROM Review;');
+		EchoQuery($mysqli, 'DELETE FROM Community;');
 	}
 
 	function PopulateFull()
@@ -32,6 +34,8 @@
 		PopulateUsers();
 		PopulateGames();
 		PopulateTracks();
+		PopulateReviews();
+		PopulateCommunities();
 		//additional populations here!
 	}
 
@@ -75,7 +79,6 @@
 	{
 		$mysqli = CreateConnection();
 		include("peuplement_tracks.php");
-		echo "<br>Peuplement de tracks... : ";
 		EchoCount($mysqli, 'Track');
 	}
 	
@@ -84,6 +87,42 @@
 		EchoQuery($mysqli, "
 			INSERT INTO Track(idGame, title, length, fadeLength, composer, turnedOffByAdmin, screenshotURL, isJingle, glicko2RD, glicko2rating, glicko2sigma, eloRating, spcURL, spcEncodedURL)
 			VALUES((SELECT idGame FROM Game WHERE titleEng = '" . addslashes($gameTitle) . "'), '" . addslashes($title) . "', '$length', '$fadeLength', '" . addslashes($composer) . "', '$turnedOffByAdmin', '" . addslashes($screenshotURL) . "', '$isJingle', '$glicko2RD','$glicko2rating','$glicko2sigma','$eloRating', 'spc/" . addslashes($spcURL) . ".spc', '$spcEncodedURL')
+		");
+	}
+
+	function PopulateReviews()
+	{
+		$mysqli = CreateConnection();
+		InsertReview($mysqli, '', '', 'This is my goddamned review!', FALSE);
+		InsertReview($mysqli, '', '', 'This track is so WHANNE!', TRUE);
+		InsertReview($mysqli, '', '', 'Good schtuff!', FALSE);
+		InsertReview($mysqli, '', '', addslashes('This track, granny, how does it feel? It\'s so good!'), TRUE);
+		InsertReview($mysqli, '', '', 'This track. I want to lick its ballz!', FALSE);
+		InsertReview($mysqli, '', '', 'My review is quite short!', TRUE);
+		EchoCount($mysqli, 'Review');
+	}
+	
+	function InsertReview($mysqli, $idUser, $idTrack, $text, $approved)
+	{
+		EchoQuery($mysqli,"
+			INSERT INTO Review(idUser, idTrack, text, approved)
+			VALUES((SELECT idUser FROM User ORDER BY RAND() LIMIT 1), (SELECT idTrack FROM Track ORDER BY RAND() LIMIT 1), '$text', '$approved')
+		");
+	}
+
+	function PopulateCommunities()
+	{
+		$mysqli = CreateConnection();
+		InsertCommunity($mysqli, 'The Shizz', 'ohhhShizz, why u always down?', 'http://theshizz.org/forum/index.php?/forum/8-minibosses-message-board/');
+		InsertCommunity($mysqli, 'LPG', 'LanPartyGuys!', 'jerther.com/~baldho/snestop/site/');
+		EchoCount($mysqli, 'Community');
+	}
+	
+	function InsertCommunity($mysqli, $name, $token, $url)
+	{
+		EchoQuery($mysqli,"
+			INSERT INTO Community(name, token, URL)
+			VALUES('$name', '$token', '$url')
 		");
 	}
 ?>
@@ -97,6 +136,8 @@
 		<p><a href="peuplement.php?action=users">Peupler les utilisateurs</a></p>
 		<p><a href="peuplement.php?action=games">Peupler les games</a></p>
 		<p><a href="peuplement.php?action=tracks">Peupler les tracks</a></p>
+		<p><a href="peuplement.php?action=reviews">Peupler les reviews</a></p>
+		<p><a href="peuplement.php?action=communities">Peupler les communities</a></p>
 		<p><a href="peuplement.php?action=full">Peuplement entier</a></p>
 		<p><a href="index.php">Retour à l'outil principal</a></p>
 		<?php
@@ -110,6 +151,10 @@
 					PopulateGames();
 				else if($_GET['action'] == 'tracks')
 					PopulateTracks();
+				else if($_GET['action'] == 'reviews')
+					PopulateReviews();
+				else if($_GET['action'] == 'communities')
+					PopulateCommunities();
 				else if($_GET['action'] == 'full')
 					PopulateFull();
 			}
