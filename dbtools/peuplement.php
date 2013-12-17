ebc24a38839e35426f69f6409e2af494f583b36b
François Gingras <juef@hotmail.com>
2013-12-17 20:32:12 +0000

    Correction d'un bug dans le peuplement des tracks (apostrophes de trop pour certains datatypes)
    git-svn-id: http://jerther.com/svn/snestop/trunk@44 ce5e4949-4d00-44cd-bea3-2e8dfc70eed9

--- dbtools/peuplement.php
+++ dbtools/peuplement.php
@@ -86,7 +86,7 @@
 	{
 		EchoQuery($mysqli, "
 			INSERT INTO Track(idGame, title, length, fadeLength, composer, turnedOffByAdmin, screenshotURL, isJingle, glicko2RD, glicko2rating, glicko2sigma, eloRating, spcURL, spcEncodedURL)
-			VALUES((SELECT idGame FROM Game WHERE titleEng = '" . addslashes($gameTitle) . "'), '" . addslashes($title) . "', '$length', '$fadeLength', '" . addslashes($composer) . "', '$turnedOffByAdmin', '" . addslashes($screenshotURL) . "', '$isJingle', '$glicko2RD','$glicko2rating','$glicko2sigma','$eloRating', 'spc/" . addslashes($spcURL) . ".spc', '$spcEncodedURL')
+			VALUES((SELECT idGame FROM Game WHERE titleEng = '" . addslashes($gameTitle) . "'), '" . addslashes($title) . "', $length, $fadeLength, '" . addslashes($composer) . "', $turnedOffByAdmin, '" . addslashes($screenshotURL) . "', $isJingle, $glicko2RD, $glicko2rating, $glicko2sigma , $eloRating, 'spc/" . addslashes($spcURL) . ".spc', '$spcEncodedURL')
 		");
 	}
 
