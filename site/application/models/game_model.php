7e332ea7a8856fc30997c0ff2672027e5806cd79
François Gingras <juef@hotmail.com>
2013-12-17 19:35:35 +0000

    Ajout du model pour Game
    git-svn-id: http://jerther.com/svn/snestop/trunk@42 ce5e4949-4d00-44cd-bea3-2e8dfc70eed9

---
+++ site/application/models/game_model.php
@@ -0,0 +1,45 @@
+<?php
+class Game_model extends CI_Model {
+	public function __construct() {
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function get_Game($idGame = FALSE) {
+		if ($idGame === FALSE) {
+			$query = $this->db->get('Game');
+			return $query->result_array();
+		} else {
+			$query = $this->db->get_where('Game', array('idGame' => $idGame));
+			return $query->row();
+		}
+	}
+
+	public function set_Game($titleJap, $titleEng, $screenshotURL, $rsnFileURL) {
+		$data = array(
+			'titleJap' => $titleJap,
+			'titleEng' => $titleEng,
+			'screenshotURL' => $screenshotURL,
+			'rsnFileURL' => $rsnFileURL
+		);
+		return $this->db->insert('Game', $data);
+	}
+
+	public function delete_Game($idGame) {
+		return $this->db->delete('Game', array('idGame' => $idGame));
+	}
+	
+	public function update_Game($idGame, $titleJap, $titleEng, $screenshotURL, $rsnFileURL) {
+		$this->db->where('Game.idGame', $idGame);
+		return $this->db->update('Game', array('titleJap' => $titleJap, 'titleEng' => $titleEng, 'screenshotURL' => $screenshotURL, 'rsnFileURL' => $rsnFileURL));
+	}
+	
+	public function switchTitles_Game($idGame) {
+		$this->db->where('Game.idGame', $idGame);
+		$query = $this->db->get('Game');
+		if($query->num_rows() == 1) {
+			$game = $query->row();
+			return $this->db->update('Game', array('titleJap' => $game->titleEng, 'titleEng' => $game->titleJap));
+		}
+	}
+}
