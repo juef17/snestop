f61bc009e8f47dd81bed357485071469182b43fe
Jérôme Thériault <jerther@gmail.com>
2015-10-13 23:46:19 +0000

    - Mise à jour des librairies JQuery et JQuery-ui pour corriger problème de modal qui ramoutz dans le window!
    git-svn-id: http://jerther.com/svn/snestop/trunk@174 ce5e4949-4d00-44cd-bea3-2e8dfc70eed9

--- site/application/views/includes/head.php
+++ site/application/views/includes/head.php
@@ -8,7 +8,7 @@
 
 <link rel="stylesheet" type="text/css" href="<?=asset_url()?>css/snestop.css">
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?=asset_url()?>js/dropdown-menu.min.js"></script>
 <script type="text/javascript" src="<?=asset_url()?>js/snestop.js"></script>
