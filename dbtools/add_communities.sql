04dd8a059a4a384528d00658bf89dde92db88456
François Gingras <juef17@hotmail.com>
2018-06-05 14:12:03 +0000

    Ajout du script SQL qui insère les communities "de base" dans la DB

---
+++ dbtools/add_communities.sql
@@ -0,0 +1,7 @@
+INSERT INTO Community(name, token, URL) VALUES('The Shizz', 'Shizzies', 'http://theshizz.org/forum/index.php?/forum/8-minibosses-message-board/');
+INSERT INTO Community(name, token, URL) VALUES('GameFAQS', 'snesLovers', 'https://gamefaqs.gamespot.com/boards/916396-super-nintendo');
+INSERT INTO Community(name, token, URL) VALUES('/r/snes', 'arr_SNES', 'https://www.reddit.com/r/retrogamingmusic');
+INSERT INTO Community(name, token, URL) VALUES('/r/retrogamingmusic', 'https://www.retroSNESmusic', 'reddit.com/r/retrogamingmusic');
+INSERT INTO Community(name, token, URL) VALUES('tasvideos', 'toolAssistedListening', 'http://tasvideos.org');
+INSERT INTO Community(name, token, URL) VALUES('byuu.org', 'SNES_Mus1c_yay', 'board.byuu.org');
+INSERT INTO Community(name, token, URL) VALUES('snesmusic.org', 'SPC_heaven', 'snesmusic.org');
